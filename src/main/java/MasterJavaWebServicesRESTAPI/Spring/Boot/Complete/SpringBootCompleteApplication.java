4fc689873ac8e30ee1181ee2f1d2f6304b0cc044
knightch <amankumarpandey2397@gmail.com>
2023-08-18 22:32:59 +0530

    Iterations 3: Loose Coupling-Spring

--- src/main/java/MasterJavaWebServicesRESTAPI/Spring/Boot/Complete/SpringBootCompleteApplication.java
+++ src/main/java/MasterJavaWebServicesRESTAPI/Spring/Boot/Complete/SpringBootCompleteApplication.java
@@ -6,16 +6,19 @@ import MasterJavaWebServicesRESTAPI.Spring.Boot.Complete.game.PacmanGame;
 import MasterJavaWebServicesRESTAPI.Spring.Boot.Complete.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootCompleteApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootCompleteApplication.class, args);
-		MarioGame game0 = new MarioGame();
-		SuperContraGame game2 = new SuperContraGame();
-		PacmanGame game1 = new PacmanGame();
-		GameRunner runner = new GameRunner(game0);
+
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootCompleteApplication.class, args);
+//		MarioGame game0 = new MarioGame();
+//		SuperContraGame game2 = new SuperContraGame();
+//		PacmanGame game1 = new PacmanGame();
+//		GameRunner runner = new GameRunner(game0);
+		GameRunner runner = context.getBean(GameRunner.class);
 		runner.run();
 	}
 
--- src/main/java/MasterJavaWebServicesRESTAPI/Spring/Boot/Complete/game/MarioGame.java
+++ src/main/java/MasterJavaWebServicesRESTAPI/Spring/Boot/Complete/game/MarioGame.java
@@ -1,5 +1,8 @@
 package MasterJavaWebServicesRESTAPI.Spring.Boot.Complete.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MarioGame  implements GamingConsole{
 
     public void up(){
